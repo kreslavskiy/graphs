f87a0741a103e1c31097d88040bdbea64d4266fd
Михайло Креславський <89911844+kreslavskiy@users.noreply.github.com>
2022-05-06 18:23:03 +0300

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,148 @@
+'use strict';
+
+const readline = require('readline');
+const vm = require('vm');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  line = line.trim();
+  const command = commands[line];
+  if (command) command();
+  else console.log('\x1b[31m', 'Unknown command', '\x1b[0m');
+  rl.prompt();
+});
+
+rl.on('close', () => process.exit(0));
+
+const question = (str) => new Promise((answer) => rl.question(str, answer));
+
+class Vertex {
+  constructor (graph, data) {
+    this.graph = graph;
+    this.data = data;
+    this.links = new Map();
+  }
+
+  link(...args) {
+    const distinct = new Set(args);
+    const { links } = this;
+    const { keyField } = this.graph;
+    for (const item of distinct) {
+      const key = item.data[keyField];
+      links.set(key, item);
+    }
+    return this;
+  }
+}
+
+class Cursor {
+  constructor(vertices) {
+    this.vertices = vertices;
+  }
+
+  linked(...names) {
+    const { vertices } = this;
+    const result = new Set();
+    for (const vertex of vertices) {
+      let condition = true;
+      for (const name of names) {
+        condition = condition && vertex.links.has(name);
+      }
+      if (condition) result.add(vertex);
+    }
+    return new Cursor(result);
+  }
+}
+
+let graph  = {
+  
+  keyField: undefined,
+  vertices: new Map(),
+
+  async keyFieldSetter () {
+    const keyField = await question ('Enter key field: ');
+    graph.keyField = keyField;
+    return keyField;
+  },
+
+  async add () {
+    const input = await question ('Enter data: ');
+    const inputExec = '{' + input + '}';
+    const script = vm.createScript('(' + inputExec + ')');
+    const data = script.runInThisContext();
+    console.dir(data);
+    const vertex = new Vertex(this, data);
+    const key = data[graph.keyField];
+    if (graph.vertices.get(key) === undefined) {
+      graph.vertices.set(key, vertex);
+    }
+    return vertex;
+  },
+
+  link (source) {
+    const { vertices } = this;
+    const from = vertices.get(source);
+    return {
+      to(...destinations) {
+        if (from) {
+          destinations.forEach(destination => {
+            const target = vertices.get(destination);
+            if (target) from.link(target);
+          });
+        }
+      }
+    };
+  },
+
+  select(query) {
+    const vertices = new Set();
+    for (const vertex of this.vertices.values()) {
+      let condition = true;
+      const { data } = vertex;
+      if (data) {
+        for (const field in query) {
+          condition = condition && data[field] === query[field];
+        }
+        if (condition) vertices.add(vertex);
+      }
+    }
+    return new Cursor(vertices);
+  },
+
+  showData () {
+    console.dir ( graph.vertices.data );
+  },
+
+  insert(records) {
+    for (const record of records) {
+      this.add(record);
+    }
+  }
+}
+
+const commands = {
+  help () {
+    console.log('Commands:', Object.keys(commands).join(', '));
+  },
+  setkf () {
+    graph.keyFieldSetter ();
+  },
+  add () {
+    graph.add ();
+  },
+  async link () {
+    const linkFrom = await question ('');
+    const linkTo = await question ('');
+    graph.link(linkFrom).to(linkTo);
+  },
+  show () {
+    graph.showData();
+  },
+  exit () {
+    rl.close();
+  },
+};
